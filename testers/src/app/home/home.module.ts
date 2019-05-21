60320de0938c6e1e4f7af6c6b7ccc995235fc6e8
Malik Harrison <harrmalik@gmail.com>
2019-05-20 21:20:01 -0400

    basic start

--- testers/src/app/home/home.module.ts
+++ testers/src/app/home/home.module.ts
@@ -5,8 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
-import { PersonsComponent } from '../persons/persons.component';
-import { PersonsInputComponent } from '../persons/persons-input.component';
 
 @NgModule({
   imports: [
@@ -20,6 +18,6 @@ import { PersonsInputComponent } from '../persons/persons-input.component';
       }
     ])
   ],
-  declarations: [HomePage, PersonsComponent, PersonsInputComponent]
+  declarations: [HomePage]
 })
 export class HomePageModule {}
